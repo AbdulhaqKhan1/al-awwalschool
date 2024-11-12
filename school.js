d3f5a90ea19e926d03b9ae0e93be526fe4bd436b
AbdulhaqKhan1 <abdulhaqk113@gmail.com>
2024-11-12 22:40:12 +0100

    Add files via upload

---
+++ school.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    document.querySelector('.spinner').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('message').style.display = 'block';
+}, 7000);
